13d3b17bce24dc705d596c1f5a6c2e0267a6ada3
demo <mr.tienpro@gmail.com>
2018-04-07 11:05:40 +0700

    update form

--- demon/Form1.Designer.cs
+++ demon/Form1.Designer.cs
@@ -28,18 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtso1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtso2 = new System.Windows.Forms.TextBox();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtso1
             // 
-            this.textBox1.Location = new System.Drawing.Point(142, 54);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(230, 22);
-            this.textBox1.TabIndex = 0;
+            this.txtso1.Location = new System.Drawing.Point(142, 54);
+            this.txtso1.Name = "txtso1";
+            this.txtso1.Size = new System.Drawing.Size(230, 22);
+            this.txtso1.TabIndex = 0;
             // 
             // label1
             // 
@@ -59,22 +62,50 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "số 2 :";
             // 
-            // textBox2
+            // txtso2
             // 
-            this.textBox2.Location = new System.Drawing.Point(142, 128);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(230, 22);
-            this.textBox2.TabIndex = 0;
+            this.txtso2.Location = new System.Drawing.Point(142, 128);
+            this.txtso2.Name = "txtso2";
+            this.txtso2.Size = new System.Drawing.Size(230, 22);
+            this.txtso2.TabIndex = 0;
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(142, 197);
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(230, 22);
+            this.txtkq.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "kết quả:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(659, 461);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.txtso2);
+            this.Controls.Add(this.txtso1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -84,10 +115,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtso1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtso2;
+        private System.Windows.Forms.TextBox txtkq;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
